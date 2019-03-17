28c503fa0f3d26a30bb76a06179450943c59c230
Marco Zakaria <marcozakaria@yahoo.com>
2019-03-18 00:28:26 +0200

    touch controls

--- Assets/Scripts/StickyBall.cs
+++ Assets/Scripts/StickyBall.cs
@@ -6,6 +6,9 @@ public class StickyBall : MonoBehaviour
 {
     public float facingAngle = 0;
 
+    public float horizontalSpeed = 100;
+    public float verticalSpeed = 500;
+
     private float x = 0, z = 0;
     private Rigidbody ballRigidbody;
     Vector2 movment;
@@ -15,6 +18,16 @@ public class StickyBall : MonoBehaviour
 
     float ballSize = 1;
 
+    //touch input
+    [Header("Touch input")]
+    public GameObject outerCircle;
+    public GameObject innerDot;
+    private Touch oneTouch;
+    private Vector2 touchPosition, touchDirection;
+    private RectTransform innerTransform, outerTransorm;
+    private Camera mainCamera;
+
+    [Space(10)]
     public GameObject category1;
     bool category1Unlocked = false;
     public GameObject category2;
@@ -25,15 +38,78 @@ public class StickyBall : MonoBehaviour
     private void Start()
     {
         ballRigidbody = GetComponent<Rigidbody>();
+
+        innerTransform = innerDot.GetComponent<RectTransform>();
+        outerTransorm = outerCircle.GetComponent<RectTransform>();
+        outerCircle.SetActive(false);
+        innerDot.SetActive(false);       
+
+        mainCamera = Camera.main;
     }
 
     private void Update()
     {
-        x = Input.GetAxis("Horizontal") * Time.deltaTime * -100;
-        z = Input.GetAxis("Vertical") * Time.deltaTime * 500;
+#if UNITY_EDITOR
+        x = Input.GetAxis("Horizontal") * Time.deltaTime * -horizontalSpeed;
+        z = Input.GetAxis("Vertical") * Time.deltaTime * verticalSpeed;
+#else
+         TouchInput();
+        x = touchDirection.x;
+        z = touchDirection.y;
+#endif
+
 
         facingAngle += x;
         movment = new Vector2(Mathf.Cos(facingAngle * Mathf.Deg2Rad), Mathf.Sin(facingAngle * Mathf.Deg2Rad));
+    }
+
+    private void TouchInput()
+    {
+        if (Input.touchCount > 0)
+        {
+            //Debug.Log("touch");
+            oneTouch = Input.GetTouch(0);
+            touchPosition = mainCamera.ScreenToViewportPoint(oneTouch.position);
+            touchPosition = new Vector2(touchPosition.x * Screen.width, touchPosition.y * Screen.height);
+
+            switch (oneTouch.phase)
+            {
+                case TouchPhase.Began:
+                    outerCircle.SetActive(true);
+                    innerDot.SetActive(true);
+                    Debug.Log(touchPosition);
+                    outerTransorm.position = touchPosition;
+                    innerTransform.position = touchPosition;
+                    break;
+                case TouchPhase.Moved:
+                    TouchMovment();
+                    break;
+                case TouchPhase.Stationary:
+                    TouchMovment();
+                    break;
+                case TouchPhase.Ended:
+                    outerCircle.SetActive(false);
+                    innerDot.SetActive(false);
+                    touchDirection = Vector2.zero;
+                    break;
+                case TouchPhase.Canceled:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    void TouchMovment()
+    {
+        innerDot.transform.position = touchPosition;
+
+        innerDot.transform.position = new Vector2(
+            Mathf.Clamp(innerTransform.position.x, outerTransorm.position.x - 60, outerTransorm.position.x + 60),
+            Mathf.Clamp(innerTransform.position.y, outerTransorm.position.y - 60, outerTransorm.position.y + 60));
+
+        touchDirection = (innerTransform.position - outerTransorm.position).normalized;
+
     }
 
     private void FixedUpdate()
@@ -42,7 +118,7 @@ public class StickyBall : MonoBehaviour
 
        cameraTransform.position = new Vector3(-movment.x * distanceToCamera, distanceToCamera, -movment.y * distanceToCamera) + this.transform.position;
 
-        unlockPickupCategories();
+        //unlockPickupCategories();
     }
 
     private void unlockPickupCategories()
